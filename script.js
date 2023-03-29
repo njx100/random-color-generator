4d7ad80c6852d40aae88480e64abb544b169d182
njx100 <dnnguyen100@gmail.com>
2023-03-29 15:32:03 +0700

    succesfully added color container using js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const containerEl = document.querySelector(".container");
 
+for (let index = 0; index < 30; index++) {
+    const colorContainerEl = document.createElement("div");
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl);
+}
